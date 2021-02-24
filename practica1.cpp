0e608c4fc5278b39d140dd651a9c5e5ce23788ea
Daniel Vazquez <programacion31022@gmail.com>
2021-02-23 22:33:41 -0600

    App Operaciones Basicas

--- practica1.cpp
+++ practica1.cpp
@@ -1,8 +1,19 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    cout<<"teclea: ";
-    int a;
-    cin>>a;
+int main()
+{
+    int n1=0,n2=0,resultado=0;
+    cout<<"Ingrese un numero: ";
+    cin>> n1;
+    cout<<"Ingrese otro numero: ";
+    cin>> n2;
+    resultado=n1+n2;
+    cout<<"Suma de "<<n1<<" y "<<n2<<" : "<<resultado<<endl;
+    resultado=n1-n2;
+    cout<<"Resta de "<<n1<<" y "<<n2<<" : "<<resultado<<endl;
+    resultado=n1*n2;
+    cout<<"Multiplicacion de "<<n1<<" y "<<n2<<" : "<<resultado<<endl;
+    resultado=n1/n2;
+    cout<<"Division de "<<n1<<" y "<<n2<<" : "<<resultado<<endl;
     return 0;
 }
